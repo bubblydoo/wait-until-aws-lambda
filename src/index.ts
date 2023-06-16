70432a2dcb6e7f7c9f8fb9876e9cfd49278b03c5
Hans Otto Wirtz <hansottowirtz@gmail.com>
2023-06-16 09:55:43 +0200

    Use wait-until-generalized

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
 export * from "./wait-until-wrapper.js";
-export * from "./wait-until-list.js";
-export * from "./wait-until-all.js";
--- src/wait-until-wrapper.ts
+++ src/wait-until-wrapper.ts
@@ -1,20 +1,25 @@
-import { WaitUntilList } from "./wait-until-list.js";
+import { WaitUntilList } from "wait-until-generalized";
 import { Context } from "aws-lambda";
 
-let waitUntilList = new WaitUntilList();
+let waitUntilList: WaitUntilList | null = null;
 
+// this handler is not safe for concurrent invocations
+// but that doesn't happen in lambdas
 export function waitUntilHandler<TEvent = any, TResult = any>(
   handler: (event: TEvent, context: Context) => Promise<TResult>
 ) {
   return async function (event: any, context: any) {
     try {
+      waitUntilList = new WaitUntilList();
       return await handler(event, context);
     } finally {
-      await waitUntilList.waitUntilFinished();
-      waitUntilList = new WaitUntilList();
+      await waitUntilList!.waitUntilSettled();
+      waitUntilList = null;
     }
   };
 }
 
-export const waitUntil = (promise: Promise<any>) =>
+export const waitUntil = (promise: Promise<any>) => {
+  if (!waitUntilList) throw new Error("waitUntilList is not initialized, is a lambda handler wrapped with waitUntilHandler?");
   waitUntilList.waitUntil(promise);
+}
